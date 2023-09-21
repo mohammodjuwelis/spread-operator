19f09d275613c39df4eb7f1d88a35d2897a2d067
mohammodjuwelis <mohammodjuwelislam@gmail.com>
2023-09-21 14:29:43 +0600

    first commit

---
+++ spread-operator.js
@@ -0,0 +1,7 @@
+// Max method spread operator
+const max = Math.max(50, 60, 100, 200, 55, 80);
+console.log(max);
+
+const numbers = [10, 20, 30, 80, 40, 70, 60];
+const arrMax = Math.max(numbers);
+console.log(arrMax);
